d9fa45827fcaa18981ab9d211dca4339f64f2ddb
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-10-27 03:35:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+const CourseData = require("./Course-Data/CourseInfo.json")
+
+
+
+app.get('/', (req, res) => {
+  res.send(CourseData)
+})
+
+app.get('/allcourses', (req, res) => {
+    res.send(CourseData)
+})
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = CourseData?.find((course) => course.id == id);
+  res.send(singleCourse);
+});
+
+
+
+
+app.listen(Port, () => {
+    console.log('Server is running on port:', Port);
+});
